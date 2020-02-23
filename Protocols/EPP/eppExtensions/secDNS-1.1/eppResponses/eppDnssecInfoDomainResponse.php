5b6119c3363b6b501d486586eed736ff93792b8d
Mark de Bruijne <m.debruijne@colours.nl>
2020-02-23 22:32:43 +0100

    Deleted local overwritten getKeydata() method which hided base implementation

--- Protocols/EPP/eppExtensions/secDNS-1.1/eppResponses/eppDnssecInfoDomainResponse.php
+++ Protocols/EPP/eppExtensions/secDNS-1.1/eppResponses/eppDnssecInfoDomainResponse.php
@@ -5,43 +5,6 @@ namespace Metaregistrar\EPP;
 class eppDnssecInfoDomainResponse extends eppInfoDomainResponse
 {
     /**
-     * Retrieve the keyData elements from the info response
-     *
-    <secDNS:dsData>
-        <secDNS:keyTag>12345</secDNS:keyTag>
-        <secDNS:alg>3</secDNS:alg>
-        <secDNS:digestType>1</secDNS:digestType>
-        <secDNS:digest>49FD46E6C4B45C55D4AC</secDNS:digest>
-    </secDNS:dsData>
-     * @return array|null
-     */
-    public function getKeydata() {
-        // Check if dnssec is enabled on this interface
-        if ($this->findNamespace('secDNS')) {
-            $xpath = $this->xPath();
-            $result = $xpath->query('/epp:epp/epp:response/epp:extension/secDNS:infData/*');
-            $keys = array();
-            if (count($result) > 0) {
-                foreach ($result as $keydata) {
-                    /* @var $keydata \DOMElement */
-                    // Check if the keyTag element is present. If not, use getKeys()
-                    $test = $keydata->getElementsByTagName('keyTag');
-                    if ($test->length > 0) {
-                        $secdns = new eppSecdns();
-                        $secdns->setKeytag($keydata->getElementsByTagName('keyTag')->item(0)->nodeValue);
-                        $secdns->setAlgorithm($keydata->getElementsByTagName('alg')->item(0)->nodeValue);
-                        $secdns->setDigestType($keydata->getElementsByTagName('digestType')->item(0)->nodeValue);
-                        $secdns->setDigest($keydata->getElementsByTagName('digest')->item(0)->nodeValue);
-                        $keys[] = $secdns;
-                    }
-                }
-            }
-            return $keys;
-        }
-        return null;
-    }
-
-    /**
      * Retrieve the keyTag elements from the info response
      *
     <secDNS:infData>
